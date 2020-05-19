582721518e0b3d4293d31a61564553a90fc8fed8
SKaralius <k.simonas.k@gmail.com>
2020-05-19 15:28:34 +0300

    Added responsiveness, credits and a message for mobile users.

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ const albums = [
   { name: "The Placeholder Blues, Vol. 10", cover: "img/placeholder.jpg" },
 ];
 
-const lorem =
-  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad veniam cupiditate excepturi quod? Accusamus, animi sapiente et minus dolorem.";
+const lorem = `This website is designed by Masudur Rahman <a href="https://dribbble.com/uigeek/projects/983583-Music-Desktop-App">checkout his Dribbble page</a> for more creative designs.
+</br>
+Implemented by Simonas Karalius, <a href="https://github.com/SKaralius">checkout my Github</a> for more projects.`;
 
 const artists = [
   { name: "Barry Blue", description: lorem, tags: ["Pop", "Glam Rock"] },
@@ -28,6 +29,37 @@ const artistHeading = document.getElementsByClassName("artist-heading")[0];
 const chevrons = document.getElementsByClassName("album-control");
 
 makeAlbums();
+
+let mainContent = "";
+
+window.onload = () => {
+  mainContent = document.getElementsByTagName("main")[0].innerHTML;
+  checkDimensions();
+};
+
+window.addEventListener("resize", () => {
+  checkDimensions();
+});
+
+function checkDimensions() {
+  let main = document.getElementsByTagName("main")[0];
+  if (window.innerWidth < 900 || window.innerHeight < 550) {
+    main.innerHTML = `
+    <div class='mobile'>
+        <h1>This is a showcase of a desktop application and it does not support mobile screens.</h1>
+        <i class="fas fa-sad-tear fa-5x"></i>
+        <a href="https://github.com/SKaralius">Checkout my Github for other projects.</a>
+        <a href="https://ksimo.com">Visite my website for more about me.</a>
+    </div>
+    `;
+  } else if (
+    window.innerWidth > 900 &&
+    window.innerHeight > 550 &&
+    main.innerHTML != mainContent
+  ) {
+    main.innerHTML = mainContent;
+  }
+}
 
 function makeTags(tags) {
   let generatedTags = "";
